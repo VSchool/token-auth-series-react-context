fb46be406300575413b12286edadea0c871a3a44
Bob Ziroll <elderziroll@gmail.com>
2020-08-26 14:16:34 -0600

    Fixing issues with index file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
+require("dotenv").config()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
+const expressJwt = require("express-jwt")
 const PORT = process.env.PORT || 5000
 
 app.use(morgan("dev"))
@@ -17,11 +19,19 @@ mongoose.connect(
         console.log("Connected to the database")
     }
 )
-
-app.use("/todo", require("./routes/todo"))
+app.use(
+    "/api",
+    expressJwt({ secret: process.env.SECRET, algorithms: ["HS256"] })
+)
+app.use("/auth", require("./routes/auth"))
+app.use("/api/todo", require("./routes/todo"))
 
 app.use((err, req, res, next) => {
     console.error(err)
+    if (err.name === "UnauthorizedError") {
+        // express-jwt gives the 401 status to the err object for us
+        res.status(err.status)
+    }
     return res.send({ message: err.message })
 })
 
