51775d0378ab0aa1809c2639bfd758b1af550f61
Bob Ziroll <elderziroll@gmail.com>
2020-08-26 12:17:57 -0600

    Undoing changes from v-school user 'add' commit

--- index.js
+++ index.js
@@ -10,21 +10,19 @@ app.use(express.json());
 //connect to db
 mongoose.set('useCreateIndex', true);
 mongoose.connect("mongodb://localhost:27017/todo-auth-example",
-{ useNewUrlParser: true },
-(err) => {
-    if (err) throw err;
-    console.log("Connected to the database");
-}
+    { useNewUrlParser: true },
+    (err) => {
+        if (err) throw err;
+        console.log("Connected to the database");
+    }
 );
 
-app.use("/api", expressJwt({secret: process.env.SECRET}))
-app.use('/auth', require('./routes/auth'))
-app.use("/api/todo", require("./routes/todo"));
+app.use("/todo", require("./routes/todo"));
 
-// app.use((err, req, res, next) => {
-//     console.error(err);
-//     return res.send({ message: err.message });
-// });
+app.use((err, req, res, next) => {
+    console.error(err);
+    return res.send({ message: err.message });
+});
 
 app.listen(PORT, () => {
     console.log(`[+] Starting server on port ${PORT}`);
